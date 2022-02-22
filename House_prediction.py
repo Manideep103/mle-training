2a1ed850440427824a66d4de7e9107ee719a8c86
Manideep103 <saimanideep.pill@tigeranalytics.com>
2022-02-22 14:04:30 +0530

    final commit

--- House_prediction.py
+++ House_prediction.py
@@ -7,14 +7,14 @@ import numpy as np
 import pandas as pd
 from six.moves import urllib
 
-DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
 
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     os.makedirs(housing_path, exist_ok=True)
-    tgz_path = os.path.join(housing_path, "housing.tgz")
+    tgz_path = os.path.join(housing_path, "housing.tgz").replace("\\", "/")
     urllib.request.urlretrieve(housing_url, tgz_path)
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
@@ -25,11 +25,11 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 
 
 def load_housing_data(housing_path=HOUSING_PATH):
-    csv_path = os.path.join(housing_path, "housing.csv")
-    return pd.read_csv(csv_path)
+    csv_path = os.path.join(housing_path, "housing.csv").replace("\\", "/")
+    return pd.read_csv((DOWNLOAD_ROOT + csv_path))
 
 
-housing = load_housing_data
+housing = load_housing_data()
 
 from sklearn.model_selection import train_test_split
 
